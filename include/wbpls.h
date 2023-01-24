af6306143088f1d257b25c64e8f795291a283c1d
Gabriele <yugo.amaril@gmail.com>
2023-01-24 12:39:26 +0100

    inizio

---
+++ include/wbpls.h
@@ -0,0 +1,30 @@
+#define PACKET_SIZE 8
+
+const double deffreq0msg = 2400.0;
+const double deffreq1msg = 3600.0;
+const double deffreq0sc = 4900.0;
+const double deffreq1sc = 5100.0;
+
+// Types
+typedef struct {
+  uint fmsg0, fmsg1, fsc0, fsc1;
+} Configuration;
+
+const Configuration Default = {deffreq0msg, deffreq1msg, deffreq0sc, deffreq1sc}
+
+typedef struct {
+  char data[PACKET_SIZE];
+} Pack;
+
+typedef Pack* Packet;
+
+// Function prototypes
+
+/*
+ *  init:
+ *  send: sends a packet
+ *  recv:
+ */
+void init(Configuration*);
+void send(Packet);
+Packet recv();
--- src/wbpls.c
+++ src/wbpls.c
@@ -1,3 +1,43 @@
-void say_hello(){
+#include <stdio.h>
+#include <limits.h>
+#include <math.h>
+#include "wbpls.h"
+
+const char H8[][] = {{1,1,1,1,1,1,1,1},
+                    {1,0,1,0,1,0,1,0},
+                    {1,1,0,0,1,1,0,0},
+                    {1,0,0,1,1,0,0,1},
+                    {1,1,1,1,0,0,0,0},
+                    {1,0,1,0,0,1,0,1},
+                    {1,1,0,0,0,0,1,1},
+                    {1,0,0,1,0,1,1,0}};
+
+
+uint intcos(double c) {
+  return ((cos(c)+1)*((UINT_MAX-1) >> 1));
+}
+
+void init(Configuration* conf) {
+  if(conf == NULL) {
+    conf = *DefaultConfiguration;
+  }
+}
+
+void send(Packet) {
+
+  // 1. Packet modulation (multiplies bits by cos of bit 1 and 0)
+
+  // 2. spreading code modulation (multiplies cw by cos 1 and 0 -- for spreading code)
+
+  // 3. allign and sum modulations' results
+
+  // 4. split to datagrams and transmit
+}
+
+Packet recv() {
+  return NULL;
+}
+
+int main() {
 
 }
